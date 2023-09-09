a37fa0a0ce26bf094f8bd93199c413608b1d5c0b
NeroXerith <astar8820@gmail.com>
2023-09-09 18:43:48 +0800

    Splash Screen Added

--- app/src/main/java/com/bryle_sanico/gui_calculator/MainActivity.java
+++ app/src/main/java/com/bryle_sanico/gui_calculator/MainActivity.java
@@ -2,13 +2,29 @@ package com.bryle_sanico.gui_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Intent CallMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        CallMain = new Intent(this, MainActivity.class);
+        // startActivity();
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e){
+                    throw new RuntimeException(e);
+                } finally {
+                    startActivity(CallMain);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
