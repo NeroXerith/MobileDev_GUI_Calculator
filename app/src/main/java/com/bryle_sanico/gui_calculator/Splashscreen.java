45e3e46e12da7206079801be169e6b7310fde6de
NeroXerith <astar8820@gmail.com>
2023-09-09 19:43:05 +0800

    Fixed the flow of activity

--- app/src/main/java/com/bryle_sanico/gui_calculator/Splashscreen.java
+++ app/src/main/java/com/bryle_sanico/gui_calculator/Splashscreen.java
@@ -2,13 +2,29 @@ package com.bryle_sanico.gui_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Splashscreen extends AppCompatActivity {
-
+    Intent CallMain;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splashscreen);
+        CallMain = new Intent(this, MainActivity.class);
+        // startActivity();
+        Thread timer = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                } catch (InterruptedException e){
+                    throw new RuntimeException(e);
+                } finally {
+                    startActivity(CallMain);
+                    finish();
+                }
+            }
+        };
+        timer.start();
     }
 }
