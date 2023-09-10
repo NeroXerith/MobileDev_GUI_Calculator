564643cff9fe59380f37f8539a08e5808047fa4b
NeroXerith <astar8820@gmail.com>
2023-09-10 19:38:55 +0800

    Project Complete!

--- app/src/main/java/com/bryle_sanico/gui_calculator/MainActivity.java
+++ app/src/main/java/com/bryle_sanico/gui_calculator/MainActivity.java
@@ -1,14 +1,106 @@
 package com.bryle_sanico.gui_calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tvCurrent;
+    TextView tvPrev;
+    TextView tvOperation;
+    String tmpHandler = "";
+    Boolean isEqualClickedOnce= false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tvCurrent = findViewById(R.id.tv_currentNum);
+        tvPrev = findViewById(R.id.tv_prevNum);
+        tvOperation = findViewById(R.id.tv_operation);
+    }
+
+    public void actionBtn(View view) {
+        if (isEqualClickedOnce) {
+            // Reset isEqualClickedOnce to false and clear the UI elements.
+            isEqualClickedOnce = false;
+            tvPrev.setText("");
+            tvOperation.setText("");
+            tmpHandler = "";
+        } else {
+            String buttonText = ((TextView) view).getText().toString();
+            if (Character.isDigit(buttonText.charAt(0)) || buttonText.equals(".")) {
+                tmpHandler += buttonText;
+                tvCurrent.setText(tmpHandler);
+            } else if (tvPrev.getText().toString().isEmpty() && (buttonText.equals("+") || buttonText.equals("-") || buttonText.equals("x") || buttonText.equals("÷"))) {
+                tvOperation.setText(buttonText);
+                tvPrev.setText(tmpHandler);
+                tvCurrent.setText("");
+                tmpHandler = "";
+            } else if (buttonText.equals("CE")) {
+                tvCurrent.setText("");
+                tmpHandler = "";
+            } else if (buttonText.equals("C")) {
+                tvPrev.setText("");
+                tvCurrent.setText("");
+                tvOperation.setText("");
+                tmpHandler = "";
+            } else if (buttonText.equals("=")) {
+                if(tvPrev.getText().equals("")){
+                    showToast("You cannot perform this action!");
+                } else {
+                    double n1, n2;
+                    n1 = Double.parseDouble(tvPrev.getText().toString());
+                    n2 = Double.parseDouble(tvCurrent.getText().toString());
+                    calculate(n1, n2);
+                }
+            } else if(buttonText.equals("EXIT")){
+                System.exit(0);
+            }
+        }
+    }
+    private void calculate(Double n1, Double n2){
+        double calculate;
+        String operation = tvOperation.getText().toString();
+        switch(operation){
+            case "+":
+                calculate = n1 + n2;
+                tvPrev.setText(n1+" + "+n2+" =");
+                tvCurrent.setText(String.valueOf(calculate));
+                break;
+            case "-":
+                calculate = n1 - n2;
+                tvPrev.setText(n1+" - "+n2+" =");
+                tvCurrent.setText(String.valueOf(calculate));
+                break;
+            case "x":
+                calculate = n1 * n2;
+                tvPrev.setText(n1+" x "+n2+" =");
+                tvCurrent.setText(String.valueOf(calculate));
+                break;
+            case "÷":
+                if(n1 == 0){
+                    tvPrev.setText(n1+" ÷ "+n2+" =");
+                    tvCurrent.setTextSize(40);
+                    tvCurrent.setText("Error: Division by zero!");
+                } else if (n2 == 0){
+                    tvPrev.setText(n1+" ÷ "+n2+" =");
+                    tvCurrent.setTextSize(40);
+                    tvCurrent.setText("Error: Division by zero!");
+                } else {
+                    calculate = n1 / n2;
+                    tvPrev.setText(n1+" ÷ "+n2+" =");
+                    tvCurrent.setText(String.valueOf(calculate));
+                }
+                break;
+        }
+        isEqualClickedOnce = true;
+        tvOperation.setText("");
+        tmpHandler ="";
+    }
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
